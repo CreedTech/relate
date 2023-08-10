2429a85c2513891169e8c9034cdfd22c64d161bc
creedtech <ayodeleayolaoke4@gmail.com>
2023-08-10 22:13:29 +0100

    Add partial code

---
+++ relate/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from relate.users.models import User
+from relate.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
