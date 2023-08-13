e3ff94a7c7400d917b9bdef01d56df4c9491f7d4
creedtech <ayodeleayolaoke4@gmail.com>
2023-08-13 18:46:52 +0100

    added infinite scrolling

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,11 +1,15 @@
 import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { AuthContext } from "../contexts/AuthContext";
 import { MessageModel } from "../models/Message";
 import { Message } from "./Message";
+import { ChatLoader } from "./ChatLoader";
 
 export function Chat() {
+    const [page, setPage] = useState(2);
+    const [hasMoreMessages, setHasMoreMessages] = useState(false);
     const { conversationName } = useParams();
     const [welcomeMessage, setWelcomeMessage] = useState("");
     const [messageHistory, setMessageHistory] = useState<any>([]);
@@ -33,7 +37,7 @@ export function Chat() {
                         setWelcomeMessage(data.message);
                         break;
                     case "chat_message_echo":
-                        setMessageHistory((prev: any) => prev.concat(data.message));
+                        setMessageHistory((prev: any) => [data.message, ...prev]);
                         break;
                     case "last_50_messages":
                         setMessageHistory(data.messages);
@@ -45,6 +49,31 @@ export function Chat() {
             }
         }
     );
+
+    async function fetchMessages() {
+        const apiRes = await fetch(
+            `http://127.0.0.1:8000/api/messages/?conversation=${conversationName}&page=${page}`,
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${user?.token}`
+                }
+            }
+        );
+        if (apiRes.status === 200) {
+            const data: {
+                count: number;
+                next: string | null; // URL
+                previous: string | null; // URL
+                results: MessageModel[];
+            } = await apiRes.json();
+            setHasMoreMessages(data.next !== null);
+            setPage(page + 1);
+            setMessageHistory((prev: MessageModel[]) => prev.concat(data.results));
+        }
+    }
 
     const connectionStatus = {
         [ReadyState.CONNECTING]: "Connecting",
@@ -98,9 +127,30 @@ export function Chat() {
             </button>
             <hr />
             <ul className="mt-3 flex flex-col-reverse relative w-full border border-gray-200 overflow-y-auto p-6">
-                {messageHistory.map((message: MessageModel) => (
+                <div
+                    id="scrollableDiv"
+                    className="h-[20rem] mt-3 flex flex-col-reverse relative w-full border border-gray-200 overflow-y-auto p-6"
+                >
+                    <div>
+                        {/* Put the scroll bar always on the bottom */}
+                        <InfiniteScroll
+                            dataLength={messageHistory.length}
+                            next={fetchMessages}
+                            className="flex flex-col-reverse" // To put endMessage and loader to the top
+                            inverse={true}
+                            hasMore={hasMoreMessages}
+                            loader={<ChatLoader />}
+                            scrollableTarget="scrollableDiv"
+                        >
+                            {messageHistory.map((message: MessageModel) => (
+                                <Message key={message.id} message={message} />
+                            ))}
+                        </InfiniteScroll>
+                    </div>
+                </div>
+                {/* {messageHistory.map((message: MessageModel) => (
                     <Message key={message.id} message={message} />
-                ))}
+                ))} */}
             </ul>
             {/* <ul>
                 {messageHistory.map((message: any, idx: number) => (
