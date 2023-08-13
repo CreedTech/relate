99f6e1f4f91464b34080ceb43ac904205c8db7d5
creedtech <ayodeleayolaoke4@gmail.com>
2023-08-13 18:52:54 +0100

    added pagination

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from relate.chats.api.views import ConversationViewSet
+from relate.chats.api.views import ConversationViewSet, MessageViewSet
 from relate.users.api.views import UserViewSet
 
 if settings.DEBUG:
@@ -11,6 +11,7 @@ else:
 
 router.register("users", UserViewSet)
 router.register("conversations", ConversationViewSet)
+router.register("messages", MessageViewSet)
 
 
 app_name = "api"
